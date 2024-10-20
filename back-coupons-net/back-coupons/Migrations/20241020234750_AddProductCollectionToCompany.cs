b03933b92517020dc22a7f0fae23878a3a95a651
lara44 <43456256+lara44@users.noreply.github.com>
2024-10-20 18:49:55 -0500

    [UPDATE] Ajuste migración producto y compañia

---
+++ back-coupons-net/back-coupons/Migrations/20241020234750_AddProductCollectionToCompany.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace back_coupons.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProductCollectionToCompany : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CompanyId",
+                table: "Companies",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Companies_CompanyId",
+                table: "Companies",
+                column: "CompanyId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Companies_Companies_CompanyId",
+                table: "Companies",
+                column: "CompanyId",
+                principalTable: "Companies",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Companies_Companies_CompanyId",
+                table: "Companies");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Companies_CompanyId",
+                table: "Companies");
+
+            migrationBuilder.DropColumn(
+                name: "CompanyId",
+                table: "Companies");
+        }
+    }
+}
